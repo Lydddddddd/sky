a0ed144e55866fed1bc8bc8d8d190f88e56921f1
liyaodong <10000@shu.edu.com>
2025-08-27 22:14:58 +0800

    新增菜品业务代码

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+
+
+    /**
+     * 插入口味
+     * @param dishFlavors
+     */
+    void insert(List<DishFlavor> dishFlavors);
+}
