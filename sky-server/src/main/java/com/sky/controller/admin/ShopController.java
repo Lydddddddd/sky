4718541ed1202bb24bdc15f6a27de3febadcd5d3
liyaodong <1320058298@qq.com>
2025-08-29 22:28:19 +0800

    redis配置

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关借口")
+@Slf4j
+public class ShopController {
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置店铺状态{}", status);
+        redisTemplate.opsForValue().set("SHOP_STATUS", status);
+        return Result.success();
+    }
+
+
+    /**
+     * 获取店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取店铺状态{}", status);
+        return Result.success(status);
+    }
+}
